9e93f6d54fb80d0be1a7cbd66f26d3a09b03d883
hunjoonrhee <hunjoonrhee90@gmail.com>
2025-06-14 20:06:54 +0200

    step 1: choose your model and color

---
+++ src/app/configurator.service.ts
@@ -0,0 +1,19 @@
+import { computed, inject, Injectable, signal, Signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CarModel, Color } from './models.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfiguratorService {
+  private http = inject(HttpClient);
+  readonly allModels: Signal<CarModel[]> = toSignal(
+    this.http.get<CarModel[]>('models'),
+    { initialValue: [] }
+  );
+
+  readonly currentCar = signal<CarModel | undefined>(undefined);
+
+  readonly currentCarImage = signal<string | undefined>(undefined);
+}
---
+++ src/app/step1/step1.component.ts
@@ -0,0 +1,48 @@
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CarModel, Color } from '../models.type';
+import { ConfiguratorService } from '../configurator.service';
+
+@Component({
+  selector: 'app-step1',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './step1.component.html',
+  styleUrl: './step1.component.scss',
+})
+export class Step1Component {
+  readonly configuratorService = inject(ConfiguratorService);
+  readonly allModels = this.configuratorService.allModels();
+
+  readonly carModel = new FormControl<string>('');
+  readonly carColor = new FormControl<string>('');
+
+  constructor() {
+    this.carModel.valueChanges.subscribe((m) => {
+      console.log(m);
+      const currentModel = this.allModels.find(
+        (model) => model.description === m
+      );
+
+      if (currentModel) {
+        this.configuratorService.currentCar.set(currentModel);
+        const firstColor = currentModel.colors[0];
+        if (firstColor) {
+          this.carColor.setValue(firstColor.code);
+        }
+      }
+    });
+
+    this.carColor.valueChanges.subscribe((c) => {
+      const currentCar = this.configuratorService.currentCar();
+      const currentColor = this.configuratorService
+        .currentCar()
+        ?.colors.find((color) => color.code === c);
+
+      if (currentColor && currentCar) {
+        const imageUrl = `https://interstate21.com/tesla-app/images/${currentCar.code}/${currentColor.code}.jpg`;
+        this.configuratorService.currentCarImage.set(imageUrl);
+      }
+    });
+  }
+}
