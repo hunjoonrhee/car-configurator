9d4ec8afc146e9b104c2ece1dae7b5a1ea2cc7fb
hunjoonrhee <hunjoonrhee90@gmail.com>
2025-06-19 21:29:02 +0200

    .

--- src/app/step1/step1.component.ts
+++ src/app/step1/step1.component.ts
@@ -50,35 +50,4 @@ export class Step1Component implements OnInit {
       this.configuratorService.setCurrentCarImage(imageUrl);
     });
   }
-
-  // constructor() {
-  //   this.carModel.valueChanges.subscribe((m) => {
-  //     console.log(m);
-  //     const currentModel = this.allModels.find(
-  //       (model) => model.description === m
-  //     );
-
-  //     if (currentModel) {
-  //       this.configuratorService.currentCar.set(currentModel);
-  //       const firstColor = currentModel.colors[0];
-  //       if (firstColor) {
-  //         this.carColor.setValue(firstColor.code);
-  //         this.configuratorService.currentCarColor.set(firstColor);
-  //       }
-  //       this.configuratorService.currentCarConfig.set(undefined);
-  //     }
-  //   });
-
-  //   this.carColor.valueChanges.subscribe((c) => {
-  //     const currentCar = this.configuratorService.currentCar();
-  //     const currentColor = this.configuratorService
-  //       .currentCar()
-  //       ?.colors.find((color) => color.code === c);
-  //     this.configuratorService.currentCarColor.set(currentColor);
-  //     if (currentColor && currentCar) {
-  //       const imageUrl = `https://interstate21.com/tesla-app/images/${currentCar.code}/${currentColor.code}.jpg`;
-  //       this.configuratorService.currentCarImage.set(imageUrl);
-  //     }
-  //   });
-  // }
 }
