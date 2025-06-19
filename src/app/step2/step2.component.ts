e402756217a0c20d905bbcabd1b12ab629f5211f
hunjoonrhee <hunjoonrhee90@gmail.com>
2025-06-19 13:03:14 +0200

    checkbox formcontrol

--- src/app/step2/step2.component.ts
+++ src/app/step2/step2.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ConfiguratorService } from '../configurator.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CurrencyPipe } from '@angular/common';
@@ -10,7 +10,7 @@ import { CurrencyPipe } from '@angular/common';
   templateUrl: './step2.component.html',
   styleUrl: './step2.component.scss',
 })
-export class Step2Component {
+export class Step2Component implements OnInit {
   service = inject(ConfiguratorService);
   readonly options = this.service.currentCarOptions();
 
@@ -18,22 +18,33 @@ export class Step2Component {
     this.service.currentCarConfig()?.id.toString() || ''
   );
 
-  constructor() {
+  readonly towHitchControl = new FormControl<boolean>(false);
+
+  ngOnInit() {
     this.configControl.valueChanges.subscribe((id) => {
       if (id) {
         console.log(typeof id);
         this.service.setConfig(parseInt(id));
       }
     });
+
+    this.towHitchControl.valueChanges.subscribe((tH) => {
+      if (tH) {
+        this.service.currentCarHitchYokeOptions.update((prev) => ({
+          ...prev,
+          towHitch: tH,
+        }));
+      }
+    });
   }
 
-  toggleTowHitch(event: Event) {
-    const checked = (event.target as HTMLInputElement).checked;
-    this.service.currentCarHitchYokeOptions.update((prev) => ({
-      ...prev,
-      towHitch: checked,
-    }));
-  }
+  // toggleTowHitch(event: Event) {
+  //   const checked = (event.target as HTMLInputElement).checked;
+  //   this.service.currentCarHitchYokeOptions.update((prev) => ({
+  //     ...prev,
+  //     towHitch: checked,
+  //   }));
+  // }
   toggleYoke(event: Event) {
     const checked = (event.target as HTMLInputElement).checked;
     this.service.currentCarHitchYokeOptions.update((prev) => ({
